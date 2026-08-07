d99fd858b9c0cbc6e566d251650c39f3926c5ec4
python-processing-unit <darthmalware@orderofsithlords.onmicrosoft.com>
2026-08-07 04:15:00 -0400

    Improve scripts/build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,18 +2,32 @@ $originalLocation = Get-Location
 Set-Location $PSScriptRoot
 
 Write-Host -NoNewline 'Building executable...'
-uv run pyinstaller --onefile --windowed --collect-data spellchecker ../src/morgul/__main__.py *> $null
-Write-Host ' done'
+$stderrFile = [System.IO.Path]::GetTempFileName()
+uv run pyinstaller --onefile --windowed --collect-data spellchecker ../src/morgul/__main__.py > $null 2> $stderrFile
+$exitCode = $LASTEXITCODE
+if ($exitCode -eq 0) {
+    Write-Host ' done'
 
-Remove-Item ../morgul.exe
-Move-Item ./dist/__main__.exe ../morgul.exe
-$ExecutableLocation = Resolve-Path "../morgul.exe"
-Write-Host "Moved executable to $ExecutableLocation"
+    Remove-Item ../morgul.exe -ErrorAction SilentlyContinue
+    Move-Item ./dist/__main__.exe ../morgul.exe
+    $ExecutableLocation = Resolve-Path "../morgul.exe"
+    Write-Host "Moved executable to $ExecutableLocation"
+} else {
+    Write-Host 'failed'
+    Get-Content $stderrFile
+}
+Remove-Item $stderrFile
 
-Write-Host -NoNewline 'Cleaning up artifacts...'
-Remove-Item ./__main__.spec
-Remove-Item -Recurse ./build/
-Remove-Item -Recurse ./dist/
-Write-Host ' done'
+
+$artifacts =  './__main__.spec', './build/', './dist/'
+if ($artifacts | Where-Object { Test-Path $_ }) {
+    Write-Host -NoNewline 'Cleaning up artifacts...'
+    foreach ($artifact in $artifacts) {
+        if (Test-Path $artifact) {
+            Remove-Item -Recurse -Force $artifact
+        }
+    }
+    Write-Host ' done'
+}
 
 Set-Location $originalLocation
